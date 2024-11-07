1f2d58ebb44470597a62850ff4bd0e267d932d1a
Greva151 <grevaval151@gmail.com>
2024-11-07 22:08:19 +0100

    10

--- esercizi/moltiplicazione-matrici.cpp
+++ esercizi/moltiplicazione-matrici.cpp
@@ -28,19 +28,59 @@ int main(){
 			cout << "Errore: valore non valido" << endl; 
 	}while(n < 1);   
 
-	int matrice[l][m] = {0}; 
+	int matrice_prima[l][m] = {0}; 
+	int matrice_seconda[m][n] = {0};
 
 	for(int i = 0; i < l; i++)
 		for(int j = 0; j < m; j++){
 			cout << "inserisci l'elemento " << i << " " << j << " della prima matrice = "; 
-			cin >> matrice[i][j]; 
+			cin >> matrice_prima[i][j]; 
 		}
 
 	for(int i = 0; i < m; i++)
 		for(int j = 0; j < n; j++){
 			cout << "inserisci l'elemento " << i << " " << j << " della seconda matrice = "; 
-			cin >> matrice[i][j]; 			
+			cin >> matrice_seconda[i][j]; 			
 		}
 
+	int prodotto[l][n] = {0}; 
+
+	for(int i = 0; i < l; i++)
+		for(int j = 0; j < n; j++){
+			prodotto[i][j] = 1; 
+
+			for(int q = 0; q < m; q++)
+				prodotto[i][j] *= matrice_prima[i][q]; 
+
+
+			for(int q = 0; q < m; q++) 
+				prodotto[i][j] *= matrice_seconda[q][j]; 
+		}
+
+	cout << "Prima matrice:" << endl; 
+	for(int i = 0; i < l; i++){
+		for(int j = 0; j < m; j++){
+			cout << matrice_prima[i][j] << " "; 
+		}
+		cout << endl; 
+	}
+
+	cout << "Seconda matrice:" << endl; 
+	for(int i = 0; i < m; i++){
+		for(int j = 0; j < n; j++){
+			cout << matrice_seconda[i][j] << " "; 
+		}
+		cout << endl; 
+	}
+
+	cout << "Prodotto tra le matrici:" << endl; 
+	for(int i = 0; i < l; i++){
+		for(int j = 0; j < n; j++){
+			cout << prodotto[i][j] << " "; 
+		}
+		cout << endl; 
+	}
+
+	
 	return 0; 
 }
