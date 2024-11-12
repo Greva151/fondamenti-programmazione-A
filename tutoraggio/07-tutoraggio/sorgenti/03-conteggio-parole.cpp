7ab88cd61957e6f370701ee09cc01a19c87b6a7f
Greva151 <grevaval151@gmail.com>
2024-11-12 16:35:12 +0100

    21

---
+++ tutoraggio/07-tutoraggio/sorgenti/03-conteggio-parole.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#define MAX 50
+using namespace std; 
+
+int main(){
+	char stringa[MAX]; 
+
+	cout << "Inserisci una stringa: "; 
+	cin.getline(stringa, MAX); 
+
+	int couter = 0; 
+
+	for(int i = 0; i < MAX; i++)
+		if(stringa[i] == ' ' && stringa[i - 1] != ' ' && (i - 1) > 0 && stringa[i + 1] != '\0')
+			couter++; 
+
+	cout << "Il numero di parole della stringa e': " << couter << endl; 
+
+
+	return 0; 
+}
