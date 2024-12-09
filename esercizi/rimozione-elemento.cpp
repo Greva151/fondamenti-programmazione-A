d004628f56a52a33167c0cdb287ec7df96f6da49
Greva151 <grevaval151@gmail.com>
2024-12-09 22:53:57 +0100

    95

--- esercizi/rimozione-elemento.cpp
+++ esercizi/rimozione-elemento.cpp
@@ -1,6 +1,5 @@
-#include <experimental/bits/fs_path.h>
 #include <iostream>
-#include <memory>
+#include <ostream>
 using namespace std;
 
 struct Node{
@@ -40,19 +39,53 @@ int countElement(Node* list){
   return counter; 
 }
 
-void deleteIndex(Node* &list, int index){
-  if(index < 0 || index > countElement(list) - 1)
+void deleteIndex(Node* &list, int index){ 
+  if(index < 0 || index > countElement(list))
     return; 
   if(index == 0){
     Node* copy = list;
     list = list->next;  
     delete copy; 
-  } 
-
+  } else {
+    int counter = 0; 
+    Node* copy = list;
+    Node* pre = list;  
+    while(counter != index){
+      counter++; 
+      pre = copy; 
+      copy = copy->next; 
+    }
+    pre->next = copy->next; 
+    delete copy; 
+  }
 }
 
+void print(Node* list){
+  while(list != nullptr){
+    cout << list->value << " "; 
+    list = list->next; 
+  }
+}
+
+
 int main(){
-    
+  Node* list = nullptr; 
+  int n; 
+  
+  do{
+   cout << "inserisci un elemento: "; 
+   cin >> n;
+   if(n != -1)
+     addNode(list, n); 
+  }while(n != -1);  
+
+  cout << "indice dell'elemento che vuoi elimanare: "; 
+  cin >> n; 
+  deleteIndex(list, n); 
+
+  print(list); 
+  
+  deleteList(list); 
   
   return 0;
 }
